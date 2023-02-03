5ca19b16939fb04542c0a11e3ed1c79b96971832
Luka Camus <luka.camus@epitech.eu>
2023-02-03 17:21:18 +0100

    [ADD] seems to have finished strcmp

--- src/strcmp.asm
+++ src/strcmp.asm
@@ -5,19 +5,22 @@ section .text                       ; coding section
 global strcmp                       ; definition of strcmp function
     strcmp:
     xor rax, rax                    ; init the return value
+    mov rcx, 0x0
 
     .loop:
-        cmp byte [rdi], 0x0
+        cmp byte [rdi + rcx], 0x0   ; see if first str is finished
         je .end
-        cmp byte [rsi], 0x0
+        cmp byte [rsi + rcx], 0x0   ; see if second str is finished
         je .end
-        cmp [rsi], [rdi]
+        mov r8, [rsi + rcx]
+        cmp byte [rdi + rcx], r8b
         jne .end
-        inc rdi
-        inc rsi
+        inc rcx
         jmp .loop
 
     .end:
-        sub [rdi], [rsi]
-        mov rax, rdi
+        mov r8, [rdi + rcx]
+        mov r9, [rsi + rcx]
+        sub r8, r9
+        mov rax, r8
         ret                         ; return rax
