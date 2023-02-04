e955b3b3df99da6ff91c6a948048a17187631875
Luka Camus <luka.camus@epitech.eu>
2023-02-04 13:18:40 +0100

    [ADD] memmove function
    close #9

---
+++ src/memmove.asm
@@ -0,0 +1,35 @@
+; void *memmove(void *dest, void *src, size_t n);
+; dest -> value to set
+; src -> value that dest is gonna be
+; n -> size of buffer
+BITS 64                             ; architecture 64
+
+section .text                       ; coding section
+
+extern memcpy
+
+global memmove
+    memmove:
+        xor rax, rax                ; init return value to 0
+        mov rcx, rdi                ; put first param (dest) in temp register
+        sub rcx, rsi                ; substract dest address with src one
+        cmp rcx, rdx                ; compare the result with n => if <, it means that src and dest overlap in memory
+        jge .end_memcpy             ; if greater, just execute a "classic" memcpy
+        mov r9, rdx
+        jmp .other                  ; if not, do an "inversed" memcpy (start by the end instead of beginning)
+
+    .end_memcpy:
+        call memcpy wrt ..plt       ; call memcpy on rax (-> return value)
+        ret                         ; return rax
+
+    .other:
+        dec r9                      ; decrement n
+        mov r8b, byte [rsi + r9]    ; put src[n] to temp register
+        mov byte [rdi + r9], r8b    ; put dest[n] at src[n]
+        cmp r9b, 0x0                ; see if size given in param is <= 0
+        je .end_other               ; go to end
+        jmp .other                  ; go back to loop
+
+    .end_other:
+        mov rax, rdi                ; move dest to return value
+        ret                         ; return rax
--- src/strcmp.asm
+++ src/strcmp.asm
@@ -1,3 +1,6 @@
+; int strcmp(const char *s1, const char *s2);
+; s1 => rdi
+; s2 => rsi
 BITS 64                             ; architecture 64
 
 section .text                       ; coding section
@@ -14,15 +17,15 @@ global strcmp                       ; definition of strcmp function
         je .end
         cmp byte [rsi + rcx], 0x0   ; see if second str is finished
         je .end
-        mov r8b, byte [rsi + rcx]         ; set r8 at rsi[rcx]
+        mov r8b, byte [rsi + rcx]   ; set r8 at rsi[rcx]
         cmp byte [rdi + rcx], r8b   ; compare byte of rdi[rcx] and byte of r8 (=> r8b)
         jne .end                    ; go to end if condition not valid
         inc rcx                     ; increment rcx
         jmp .loop                   ; continue loop
 
     .end:
-        mov r8b, byte [rdi + rcx]         ; set r8 to rdi[rcx]
-        mov r9b, byte [rsi + rcx]         ; set r9 to rsi[rcx]
+        mov r8b, byte [rdi + rcx]   ; set r8 to rdi[rcx]
+        mov r9b, byte [rsi + rcx]   ; set r9 to rsi[rcx]
         sub r8, r9                  ; => r8 -= r9
         mov rax, r8                 ; set return value to r8
         ret                         ; return rax
